abfe834203408a7d3ba001eaf44c28efc4ad11f8
Bhoomee21 <akimoohb21@gmail.com>
2025-06-04 23:24:32 +0530

    first commit

---
+++ TEGSSS3/src/main/java/com/bhoomee21/App.java
@@ -0,0 +1,13 @@
+package com.bhoomee21;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
